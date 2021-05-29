70ef2166d01d5825ef7aa6c7dd54e32d4f429017
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2021-05-29 00:47:42 -0500

    REFACTOR: People list
    Created an object that is now in charge of taking care of which table to
    render, now that I'm working on the characters-count list.
    
    This will make easier to show the incoming tables with different data.

--- app/javascript/components/sales_loft/PeopleList.js
+++ app/javascript/components/sales_loft/PeopleList.js
@@ -1,31 +1,42 @@
-import React from "react"
+import React, { useState } from "react";
 
 function PeopleList({ people }) {
-  const peopleRows = people.map(({ id, emailAddress, title, displayName }) =>
-    <tr key={id.toString()}>
-      <td><span>Name</span>{ displayName }</td>
-      <td><span>Email</span>{ emailAddress }</td>
-      <td><span>Job Title</span>{ title }</td>
-    </tr>
+  const peopleTable = () => (
+    <table>
+      <thead>
+        <tr>
+          <th> Name </th>
+          <th> Email </th>
+          <th> Job title </th>
+        </tr>
+      </thead>
+      <tbody>
+        { people.map(({ id, emailAddress, title, displayName }) =>
+            <tr key={id.toString()}>
+              <td><span>Name</span>{ displayName }</td>
+              <td><span>Email</span>{ emailAddress }</td>
+              <td><span>Job Title</span>{ title }</td>
+            </tr>
+        )}
+      </tbody>
+    </table>
   );
+
+  const renderTable = {
+    peopleTable: {
+      title: "People",
+      table: peopleTable
+    },
+  };
+
+  const [tableToRender, setTableToRender] = useState("peopleTable");
 
   return (
     <section>
-      <h1><b>People</b></h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Job Title</th>
-          </tr>
-        </thead>
-        <tbody>
-         { peopleRows }
-        </tbody>
-      </table>
+      <h1><b>{ renderTable[tableToRender].title }</b></h1>
+      { renderTable[tableToRender].table() }
     </section>
   );
 }
 
-export default PeopleList
+export default PeopleList;
