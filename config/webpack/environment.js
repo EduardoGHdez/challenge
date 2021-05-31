e3f3d65c842da8628b8aa4f7590555907b7f4c6c
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2021-05-31 12:30:38 -0500

    Exclude tests from webpack loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.get('babel').exclude = [/\.test\.js$/, environment.loaders.get('babel').exclude];
+
 module.exports = environment
